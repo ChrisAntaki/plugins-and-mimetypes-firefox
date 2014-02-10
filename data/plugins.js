dbdd1a82209b3eeae768245cd34a2054559b9b84
Chris Antaki <ChrisAntaki@gmail.com>
2014-02-09 16:56:27 -0800

    Allow Google Hangouts

--- data/plugins.js
+++ data/plugins.js
@@ -1,6 +1,16 @@
-Object.defineProperty(unsafeWindow.navigator, 'plugins', {
-    value: []
-});
-Object.defineProperty(unsafeWindow.navigator, 'mimeTypes', {
-    value: []
-});
+(function() {
+
+    // Allow Google Hangouts.
+    if (location.host === 'plus.google.com' || location.pathname.match(/hangouts/)) {
+        return;
+    }
+
+    Object.defineProperty(unsafeWindow.navigator, 'plugins', {
+        value: []
+    });
+
+    Object.defineProperty(unsafeWindow.navigator, 'mimeTypes', {
+        value: []
+    });
+
+})();
