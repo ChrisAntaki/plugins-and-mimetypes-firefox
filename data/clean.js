63f83fac1eab347b6c10368cff849121d8e8dac1
Chris Antaki <ChrisAntaki@gmail.com>
2014-03-12 23:05:09 -0700

    New Widget icon

--- data/clean.js
+++ data/clean.js
@@ -3,23 +3,23 @@
 
 
     // Allow Google Hangouts.
-    if (location.host === 'plus.google.com' &&
+    if (location.host === "plus.google.com" &&
         location.pathname.match(/hangouts/)) {
         return;
     }
 
     // Allow Gmail.
-    if (location.host === 'mail.google.com') {
+    if (location.host === "mail.google.com") {
         return;
     }
 
     // Clear `plugin` indentifiers.
-    Object.defineProperty(unsafeWindow.navigator, 'plugins', {
+    Object.defineProperty(unsafeWindow.navigator, "plugins", {
         value: void 0
     });
 
     // Clear `mimetype` indentifiers.
-    Object.defineProperty(unsafeWindow.navigator, 'mimeTypes', {
+    Object.defineProperty(unsafeWindow.navigator, "mimeTypes", {
         value: void 0
     });
 
